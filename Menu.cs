698758eda88b408bded660c57a0af8b42a3ebe56
Tanush1912 <116386437+Tanush1912@users.noreply.github.com>
2024-03-22 14:15:15 +0000

    Update Menu.cs

--- Menu.cs
+++ Menu.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using ATMSimulator;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +11,46 @@ using System.Windows.Forms;
 
 namespace ATMSimulator
 {
-    public partial class Menu : Form
+    partial class Menu : Form
     {
+        public Account ActiveAccount { get; set; }
+        private bool useRaceCondition = false; 
+
+        private void RaceButton_Click(object sender, EventArgs e)
+        {
+            useRaceCondition = false;
+            OpenForm1();
+            
+        }
+
         public Menu()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1(ActiveAccount);
+            form1.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1(ActiveAccount);
+            form1.ShowDialog();
+        }
+
+        private void OpenForm1()
+        {
+            if (ActiveAccount != null)
+            {
+                Form1 form1 = new Form1(ActiveAccount);
+                form1.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("No active account. Please log in first.", "Error");
+            }
         }
     }
 }
