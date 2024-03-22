e8e6105b4ff62d8f034882dda05fbd706abdf8ed
Tanush1912 <116386437+Tanush1912@users.noreply.github.com>
2024-03-22 14:14:54 +0000

    Update Menu.Designer.cs

--- Menu.Designer.cs
+++ Menu.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace ATMSimulator
+namespace ATMSimulator
 {
     partial class Menu
     {
@@ -28,51 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.RaceButton = new System.Windows.Forms.Button();
+            this.NonRaceButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
-            this.panel3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // RaceButton
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(10, 4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(358, 116);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Data Race ";
-            this.button1.UseVisualStyleBackColor = false;
+            this.RaceButton.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.RaceButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RaceButton.Location = new System.Drawing.Point(5, 2);
+            this.RaceButton.Margin = new System.Windows.Forms.Padding(2);
+            this.RaceButton.Name = "RaceButton";
+            this.RaceButton.Size = new System.Drawing.Size(179, 60);
+            this.RaceButton.TabIndex = 0;
+            this.RaceButton.Text = "Data Race ";
+            this.RaceButton.UseVisualStyleBackColor = false;
+            this.RaceButton.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // NonRaceButton
             // 
-            this.button2.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(5, 6);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(350, 115);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Non Data Race";
-            this.button2.UseVisualStyleBackColor = false;
+            this.NonRaceButton.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.NonRaceButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NonRaceButton.Location = new System.Drawing.Point(2, 3);
+            this.NonRaceButton.Margin = new System.Windows.Forms.Padding(2);
+            this.NonRaceButton.Name = "NonRaceButton";
+            this.NonRaceButton.Size = new System.Drawing.Size(175, 60);
+            this.NonRaceButton.TabIndex = 1;
+            this.NonRaceButton.Text = "Non Data Race";
+            this.NonRaceButton.UseVisualStyleBackColor = false;
+            this.NonRaceButton.Click += new System.EventHandler(this.button2_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(33, 35);
+            this.label1.Location = new System.Drawing.Point(16, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(570, 84);
+            this.label1.Size = new System.Drawing.Size(305, 48);
             this.label1.TabIndex = 2;
             this.label1.Text = "Welcome to the ATM Simulator \r\nfor Dundee City Bank  ";
             // 
@@ -80,9 +85,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(54, 39);
+            this.label2.Location = new System.Drawing.Point(27, 20);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(472, 64);
+            this.label2.Size = new System.Drawing.Size(237, 31);
             this.label2.TabIndex = 3;
             this.label2.Text = "Dundee City Bank ";
             // 
@@ -93,66 +99,72 @@
             this.panel1.Controls.Add(this.panel3);
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(77, 139);
+            this.panel1.Location = new System.Drawing.Point(38, 72);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(632, 716);
+            this.panel1.Size = new System.Drawing.Size(316, 372);
             this.panel1.TabIndex = 4;
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.panel2.Controls.Add(this.button2);
-            this.panel2.Location = new System.Drawing.Point(125, 197);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(362, 127);
-            this.panel2.TabIndex = 3;
-            // 
-            // panel3
-            // 
-            this.panel3.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.panel3.Controls.Add(this.button1);
-            this.panel3.Location = new System.Drawing.Point(111, 386);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(376, 129);
-            this.panel3.TabIndex = 4;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::ATMSimulator.Properties.Resources._1000_F_183718618_tjGJvwbby5SUXfb2sNhiYUZa6xzW8SIs;
-            this.pictureBox1.Location = new System.Drawing.Point(532, 2);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(162, 131);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 5;
-            this.pictureBox1.TabStop = false;
             // 
             // pictureBox2
             // 
             this.pictureBox2.Image = global::ATMSimulator.Properties.Resources._360_F_60541009_lgEIUEPoTAe6JGcUTMdg4PaBm7KweKYG;
-            this.pictureBox2.Location = new System.Drawing.Point(3, 551);
+            this.pictureBox2.Location = new System.Drawing.Point(2, 287);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(239, 162);
+            this.pictureBox2.Size = new System.Drawing.Size(120, 84);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 5;
             this.pictureBox2.TabStop = false;
             // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel3.Controls.Add(this.RaceButton);
+            this.panel3.Location = new System.Drawing.Point(56, 201);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(188, 67);
+            this.panel3.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel2.Controls.Add(this.NonRaceButton);
+            this.panel2.Location = new System.Drawing.Point(62, 102);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(181, 66);
+            this.panel2.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ATMSimulator.Properties.Resources._1000_F_183718618_tjGJvwbby5SUXfb2sNhiYUZa6xzW8SIs;
+            this.pictureBox1.Location = new System.Drawing.Point(266, 1);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(81, 68);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
             // Menu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(800, 866);
+            this.ClientSize = new System.Drawing.Size(400, 450);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Menu";
             this.Text = "Menu";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            this.panel2.ResumeLayout(false);
-            this.panel3.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -160,8 +172,8 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button RaceButton;
+        private System.Windows.Forms.Button NonRaceButton;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Panel panel1;
